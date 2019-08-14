e207bc1a8fe59272aa28e49c8725be2a5861d97f
jy202715 <jy37110@gmail.com>
2019-08-14 16:13:17 +1200

    add web token check

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const credential = require('./credential.js');
 const AWS = credential.updateCredential();
 const db = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+const jwt = require('jsonwebtoken');
+const uuidv1 = require('uuid/v1');
 
 exports.handler = (event, context, callback) => {
     if(event.hasOwnProperty('Action')){
@@ -9,7 +11,10 @@ exports.handler = (event, context, callback) => {
                 loginHandler(event, callback);
                 break;
             case "Create User":
-                createUserHandler();
+                createUserHandler(event, callback);
+                break;
+            case "Create Invoice":
+                createInvoiceHandler(event, callback);
                 break;
             default:
                 invalidInputHandler(callback);
@@ -28,7 +33,7 @@ const loginHandler = (inputObj, callback) => {
         let params = {
             TableName: 'panda_user',
             Key: {
-                'user_id': {N: userId}
+                'user_id': {S: userId}
             }
         };
         db.getItem(params, function(err, data){
@@ -39,7 +44,8 @@ const loginHandler = (inputObj, callback) => {
                     callback({IsSuccess: false, ErrorMessage: "User id not found"});
                 } else {
                     if(data.Item.password.S === password){
-                        callback({IsSuccess: true, Data: data.Item});
+                        let token = jwt.sign({id: data.Item.user_id, name: data.Item.name},'panda_secret',{expiresIn: '30d'});
+                        callback({IsSuccess: true, Data: token});
                     } else {
                         callback({IsSuccess: false, ErrorMessage:" The Password is Invalid"});
                     }
@@ -48,7 +54,7 @@ const loginHandler = (inputObj, callback) => {
         });
     }
 };
-const createUserHandler = () => {
+const createUserHandler = (inputObj, callback) => {
 
 };
 const invalidInputHandler = (callback) => {
@@ -57,5 +63,43 @@ const invalidInputHandler = (callback) => {
         ErrorMessage: "Invalid Params"
     };
     callback(result);
+};
+
+const tokenValidator = (token) => {
+    try{
+        let decodeToken = jwt.verify(token, 'panda_secret');
+        return {IsSuccess: true, Data: decodeToken};
+    } catch {
+        return {IsSuccess: false, ErrorMessage: "Invalid Token"};
+    }
+};
+
+const createInvoiceHandler = (inputObj, callback) => {
+    let token = tokenValidator(inputObj.Token);
+    if(!token.IsSuccess){
+        callback({IsSuccess: false, ErrorMessage: token.ErrorMessage});
+    } else {
+        let params = {
+            TableName: 'invoice',
+            Item:{
+                'invoice_id': {S: uuidv1().toString()},
+                'amount_by_day': {N: inputObj.Data.amountByDay.toString()},
+                'duration': {S: inputObj.Data.duration},
+                'happened_at': {S: inputObj.Data.happenedAt},
+                'name': {S: inputObj.Data.name},
+                'total_amount': {N: inputObj.Data.totalAmount.toString()},
+                'user_id': {N: token.Data.id.N.toString()},
+                'user_name': {S: token.Data.name.S},
+                'category': {S: inputObj.Data.category}
+            }
+        };
+        db.putItem(params, function(err, data){
+            if(err){
+                callback({IsSuccess: false, ErrorMessage: err});
+            } else {
+                callback({IsSuccess: true, Data: data});
+            }
+        });
+    }
 };
 
--- test.js
+++ test.js
@@ -1,10 +1,31 @@
 let handler = require('./index.js');
 
+// let event = {
+//     "Action": "Login",
+//     "Data": {
+//         "Id" : 2,
+//         "Password": "123123"
+//     }
+// };
+
+// let event = {
+//     "Action": "Create User",
+//     "Data": {
+//         "Token": 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6eyJOIjoiMiJ9LCJuYW1lIjp7IlMiOiJwYW5kYSJ9LCJpYXQiOjE1NjU3NDM0NTgsImV4cCI6MTU2ODMzNTQ1OH0.HI1ytwVIh6JaWeT_h0HHvc_N27RF7r_xtKo9nZQlv-s',
+//
+//     }
+// };
+
 let event = {
-    "Action": "Login",
+    "Action": "Create Invoice",
+    "Token": 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6eyJOIjoiMiJ9LCJuYW1lIjp7IlMiOiJwYW5kYSJ9LCJpYXQiOjE1NjU3NDM0NTgsImV4cCI6MTU2ODMzNTQ1OH0.HI1ytwVIh6JaWeT_h0HHvc_N27RF7r_xtKo9nZQlv-s',
     "Data": {
-        "Id" : 2,
-        "Password": "123123"
+        "amountByDay": 20,
+        "category": "category",
+        "duration": "monthly",
+        "happenedAt": "2019-08-09",
+        "name": "Name",
+        "totalAmount": 300,
     }
 };
 let context = {};
